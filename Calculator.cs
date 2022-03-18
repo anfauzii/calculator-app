f1a07eaf64d38b460c59f5e9700aec8355aba629
Ahmad Nur Fauzi <ahmadnurfauzi1704@gmail.com>
2022-03-18 19:45:15 +0700

    2. Penambahan fungsi operator aritmatika

--- Calculator.cs
+++ Calculator.cs
@@ -12,5 +12,25 @@ namespace CalculatorConsoleApp
 		{
 			
 		}
+		
+		static int Penambahan(int a, int b)
+		{
+			return a + b;
+		}
+		
+		static int Pengurangan(int a, int b)
+		{
+			return a - b;
+		}
+		
+		static int Perkalian(int a, int b)
+		{
+			return a * b;
+		}
+		
+		static int Pembagian(int a, int b)
+		{
+			return a / b;
+		}	
 	}
 }
